b14f38cdbae7949d528fc8a8412556d0686cda6b
Mohammed-Zaed <zaed@tuta.io>
2022-07-15 22:28:32 +0530

    Project Setup

---
+++ Engine/src/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "test.h"
+
+namespace Engine
+{
+	void print()
+	{
+		std::cout << "Welcome to Engine" << std::endl;
+	}
+}
---
+++ sandbox/src/main.cpp
@@ -0,0 +1,10 @@
+
+namespace Engine
+{
+	__declspec(dllimport) void print();
+}
+
+void main()
+{
+	Engine::print();
+}
